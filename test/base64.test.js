acafe23c77c85e5301983c64dfa387dcbf8e5f10
uzlopak <aras.abbasi@googlemail.com>
2022-08-19 20:19:05 +0200

    add base64 tests

---
+++ test/base64.test.js
@@ -0,0 +1,14 @@
+const isEncoding = Buffer.isEncoding
+Buffer.isEncoding = function (encoding) {
+  if (encoding !== 'base64url') {
+    return isEncoding(encoding)
+  }
+  return false
+}
+
+require('./constructor.test')
+require('./create.test')
+require('./integration.test')
+require('./secret.test')
+require('./secretSync.test')
+require('./verify.test')
