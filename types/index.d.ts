2b3df80fb6594a04d51fbc367eb4bf0653518456
uzlopak <aras.abbasi@googlemail.com>
2022-12-05 02:28:33 +0100

    nodenext compatibility

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,84 +1,89 @@
-interface TokensBase {
-  /**
-   * Create a new secret key.
-   */
-   secret(callback: SecretCallback): void;
-   secret(): Promise<string>;
+interface TokensConstructor {
+  (options?: Tokens.Options & { userInfo: true}): Tokens.TokensUserinfo;
+  (options?: Tokens.Options): Tokens.TokensSimple;
 
-  /**
-   * Create a new secret key synchronously.
-   */
-  secretSync(): string;
+  new(options?: Tokens.Options & { userInfo: true}): Tokens.TokensUserinfo;
+  new(options?: Tokens.Options): Tokens.TokensSimple;
 }
 
-interface TokensSimple extends TokensBase {
-  /**
-   * Create a new CSRF token.
-   */
-  create(secret: string): string;
+declare namespace Tokens {
+  interface TokensBase {
+    /**
+     * Create a new secret key.
+     */
+     secret(callback: SecretCallback): void;
+     secret(): Promise<string>;
+  
+    /**
+     * Create a new secret key synchronously.
+     */
+    secretSync(): string;
+  }
+  
+  export interface TokensSimple extends TokensBase {
+    /**
+     * Create a new CSRF token.
+     */
+    create(secret: string): string;
+  
+    /**
+     * Verify if a given token is valid for a given secret.
+     */
+    verify(secret: string, token: string): boolean;
+  }
+  
+  export interface TokensUserinfo extends TokensBase {
+    /**
+     * Create a new CSRF token.
+     */
+    create(secret: string, userInfo: string): string;
+  
+    /**
+     * Verify if a given token is valid for a given secret.
+     */
+    verify(secret: string, token: string, userInfo: string): boolean;
+  }
+  
+  export type SecretCallback = (err: Error | null, secret: string) => void;
+  
+  export interface Options {
+    /**
+     * The algorithm used to generate the token
+     * @default sha256
+     */
+    algorithm?: string;
+  
+    /**
+     * The string length of the salt
+     * 
+     * @default 8
+     */
+    saltLength?: number;
+    /**
+     * The byte length of the secret key
+     * 
+     * @default 18
+     */
+    secretLength?: number;
+  
+    /**
+     * The maximum milliseconds of validity of this token. 0 disables the check.
+     * 
+     * @default 0
+     */
+    validity?: number;
+  
+    /**
+     * Require userInfo on create() and verify()
+     * 
+     * @default false
+     */
+    userInfo?: boolean;
+  }
 
-  /**
-   * Verify if a given token is valid for a given secret.
-   */
-  verify(secret: string, token: string): boolean;
+  export const Tokens: TokensConstructor
+  export { Tokens as default }
 }
 
-interface TokensUserinfo extends TokensBase {
-  /**
-   * Create a new CSRF token.
-   */
-  create(secret: string, userInfo: string): string;
-
-  /**
-   * Verify if a given token is valid for a given secret.
-   */
-  verify(secret: string, token: string, userInfo: string): boolean;
-}
-
-export type SecretCallback = (err: Error | null, secret: string) => void;
-
-export interface Options {
-  /**
-   * The algorithm used to generate the token
-   * @default sha256
-   */
-  algorithm?: string;
-
-  /**
-   * The string length of the salt
-   * 
-   * @default 8
-   */
-  saltLength?: number;
-  /**
-   * The byte length of the secret key
-   * 
-   * @default 18
-   */
-  secretLength?: number;
-
-  /**
-   * The maximum milliseconds of validity of this token. 0 disables the check.
-   * 
-   * @default 0
-   */
-  validity?: number;
-
-  /**
-   * Require userInfo on create() and verify()
-   * 
-   * @default false
-   */
-  userInfo?: boolean;
-}
-
-export const Tokens: TokensConstructor;
-export default Tokens;
-
-export interface TokensConstructor {
-  (options?: Options & { userInfo: true}): TokensUserinfo;
-  (options?: Options): TokensSimple;
-
-  new(options?: Options & { userInfo: true}): TokensUserinfo;
-  new(options?: Options): TokensSimple;
-}
+declare function Tokens(...params: Parameters<TokensConstructor>): ReturnType<TokensConstructor>
+export = Tokens
