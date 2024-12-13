1fe54ad421f563e502de5751bbff4c65354a76d4
Frazer Smith <frazer.dev@icloud.com>
2024-12-13 13:18:47 +0000

    build(deps-dev): replace standard with neostandard (#73)
    * build(deps-dev): replace standard with neostandard
    
    * chore: add eslint.config.js
    
    * chore: ignore no-new eslint rule
    
    * build: bump neostandard

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,46 +1,47 @@
-import { expectError, expectType } from "tsd";
-import { Tokens } from "..";
+/* eslint-disable no-new -- Testing constructor types, so no need to assign */
+import { expectError, expectType } from 'tsd'
+import { Tokens } from '..'
 
-Tokens();
-new Tokens();
-Tokens({});
-new Tokens({});
-Tokens({ algorithm: 'sha1' });
-Tokens({ algorithm: 'sha256' });
-Tokens({ saltLength: 10 });
-Tokens({ secretLength: 10 });
-Tokens({ userInfo: true });
-Tokens({ validity: 10000 });
-Tokens({ hmacKey: 'foo' });
-new Tokens({ saltLength: 10 });
-new Tokens({ secretLength: 10 });
-new Tokens({ userInfo: true });
-new Tokens({ validity: 10000 });
+Tokens()
+new Tokens()
+Tokens({})
+new Tokens({})
+Tokens({ algorithm: 'sha1' })
+Tokens({ algorithm: 'sha256' })
+Tokens({ saltLength: 10 })
+Tokens({ secretLength: 10 })
+Tokens({ userInfo: true })
+Tokens({ validity: 10000 })
+Tokens({ hmacKey: 'foo' })
+new Tokens({ saltLength: 10 })
+new Tokens({ secretLength: 10 })
+new Tokens({ userInfo: true })
+new Tokens({ validity: 10000 })
 
 expectError(Tokens('secret'))
 expectError(new Tokens('secret'))
 
-expectError(new Tokens({}).create('secret', 'userInfo'));
-expectError(new Tokens({ userInfo: false}).create('secret', 'userInfo'));
-expectError(new Tokens({ userInfo: true }).create('secret'));
+expectError(new Tokens({}).create('secret', 'userInfo'))
+expectError(new Tokens({ userInfo: false }).create('secret', 'userInfo'))
+expectError(new Tokens({ userInfo: true }).create('secret'))
 
-expectError(new Tokens({}).verify('secret', 'token', 'userinfo'));
-expectError(new Tokens({ userInfo: false}).verify('secret', 'token', 'userInfo'));
-expectError(new Tokens({ userInfo: true }).verify('secret', 'token'));
+expectError(new Tokens({}).verify('secret', 'token', 'userinfo'))
+expectError(new Tokens({ userInfo: false }).verify('secret', 'token', 'userInfo'))
+expectError(new Tokens({ userInfo: true }).verify('secret', 'token'))
 
-expectError(new Tokens({ hmacKey: 123 }));
+expectError(new Tokens({ hmacKey: 123 }))
 
-expectType<Promise<string>>(Tokens().secret());
-expectType<Promise<string>>(new Tokens().secret());
+expectType<Promise<string>>(Tokens().secret())
+expectType<Promise<string>>(new Tokens().secret())
 
-expectType<void>(Tokens().secret((err, secret ) => {
-  expectType<Error| null>(err)
+expectType<void>(Tokens().secret((err, secret) => {
+  expectType<Error | null>(err)
   expectType<string>(secret)
-}));
-expectType<void>(new Tokens().secret((err, secret ) => {
-  expectType<Error| null>(err)
+}))
+expectType<void>(new Tokens().secret((err, secret) => {
+  expectType<Error | null>(err)
   expectType<string>(secret)
-}));
+}))
 
 expectType<string>(Tokens().secretSync())
 expectType<string>(new Tokens().secretSync())
